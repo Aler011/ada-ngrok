cdd9cc160ece5cf2a15706378aeae0bc14315c90
Alexis Rosaldo <60054142+Aler011@users.noreply.github.com>
2025-02-23 19:45:10 -0600

    Add files via upload

---
+++ script_ngrok.sh
@@ -0,0 +1,17 @@
+# Deteniendo  nginx y ngrok
+echo "Deteniendo nginx y ngrok!"
+sudo systemctl stop nginx
+pkill ngrok
+
+# Actualizando el repositorio
+echo "Actualizando el repositorio"
+cd /var/www/ada-perfil-profesional
+git pull origin main         
+
+# Encender nginx
+echo "Encendiendo nginx!"
+sudo systemctl start nginx
+
+#generar url
+echo "Generando url de ngrok!"
+ngrok http 80
