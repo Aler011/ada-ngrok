927c8be5a57543fe777f9930337672b20ab3b1e3
Alexis Rosaldo <60054142+Aler011@users.noreply.github.com>
2025-02-23 20:22:08 -0600

    Update script_ngrok.sh

--- script_ngrok.sh
+++ script_ngrok.sh
@@ -1,4 +1,4 @@
-# Deteniendo  nginx y ngrok
+# Deteniendo nginx y ngrok
 echo "Deteniendo nginx y ngrok!"
 sudo systemctl stop nginx
 pkill ngrok
@@ -6,12 +6,12 @@ pkill ngrok
 # Actualizando el repositorio
 echo "Actualizando el repositorio"
 cd /var/www/ada-perfil-profesional
-git pull origin main         
+git pull origin main
 
 # Encender nginx
 echo "Encendiendo nginx!"
 sudo systemctl start nginx
 
-#generar url
+# Generar url
 echo "Generando url de ngrok!"
 ngrok http 80
