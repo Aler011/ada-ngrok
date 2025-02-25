decd4e650e8d22ca9c15ac59609af63f3798face
Alexis Rosaldo <60054142+Aler011@users.noreply.github.com>
2025-02-25 14:19:39 -0600

    Update script_ngrok.sh

--- script_ngrok.sh
+++ script_ngrok.sh
@@ -14,4 +14,11 @@ sudo systemctl start nginx
 
 # Generar url
 echo "Generando url de ngrok!"
-ngrok http 80
+ngrok http 80 > /dev/null &
+
+# Esperando inicio de ngrok
+sleep 5
+
+# Obtener la URL de NGROK
+NGROK_URL=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+echo "url de ngrok: $NGROK_URL"
